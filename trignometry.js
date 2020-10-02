ee1cb338b51478ff2500a90379bb1d1b3b23d72b
SJMJ <48629086+SJMJ1929@users.noreply.github.com>
2020-10-02 12:50:50 +0530

    Create trignometry.js

---
+++ trignometry.js
@@ -0,0 +1,10 @@
+function sinfn() 
+{ 
+   let x = document.getElementById("InputNum").value    
+   document.getElementById("result").value = Math.sin(x); 
+} 
+function cosfn() 
+{ 
+   let x = document.getElementById("InputNum").value    
+   document.getElementById("result").value = Math.cos(x); 
+} 
